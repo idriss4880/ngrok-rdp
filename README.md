80e85ed7b206d01bc39114158cf4ee6715844c72
Titas Ahmed <adtitas2019@cseku.ac.bd>
2022-04-16 21:25:03 +0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+![Visitors](https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fgithub.com%2Fadtitas%2Fngrok-rdp&labelColor=%2314213d&countColor=%23e5e5e5&style=flat-square)
+
+## Description
+**What is RDP?**<br>
+* RDP (Remote Desktop Protocol) is a network communications protocol developed by Microsoft, which allows users to connect to another computer from a remote location.
+
+**How long does this RDP stay active?**<br>
+* This RDP stays active for up to 6 hours.
+
+## Screenshots
+<img src="https://i.imgur.com/vgD2owk.png" alt="ss" width="90%"/>
+<img src="https://i.imgur.com/8XBLUqf.png" alt="ss" width="90%"/>
+
+## License
+The content of this project itself is licensed under the [Creative Commons Attribution 3.0 Unported license](https://creativecommons.org/licenses/by/3.0/), and the underlying source code used to format and display that content is licensed under the [MIT License](LICENSE.md).
---
+++ Resources/start.bat
@@ -0,0 +1,21 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By adtitas" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
+net user adtitas 200219 /add >nul
+net localgroup administrators adtitas /add >nul
+net user adtitas /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant adtitas:F >nul
+ICACLS C:\Windows\installer /grant adtitas:F >nul
+echo Successfully Installed !, If the RDP is Dead, Please Rebuild Again!
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+echo Username: adtitas
+echo Password: 200219
+echo You Can Login Now
+ping -n 10 127.0.0.1 >nul
